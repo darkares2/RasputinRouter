bf3cd940c09340a7a8252f34730faf288b126f0b
rasputin <thomas.1.andersen@gmail.com>
2023-04-02 15:44:20 +0200

    Added timing logs

--- RasputinRouter/QueueTriggerRouter.cs
+++ RasputinRouter/QueueTriggerRouter.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Text.Json;
@@ -22,18 +23,52 @@ namespace Rasputin.Router
             {
                 PropertyNamingPolicy = JsonNamingPolicy.CamelCase
             });
+            await SendLog(message);
+            
             message.Headers.FirstOrDefault(x => {
                     x.Fields.TryGetValue("Active", out string active);
                     return x.Name == "route-header" && active.ToLower() == "true";
                 }).Fields.TryGetValue("Destination", out string queueName);
             if (queueName != null) {
                 log.LogInformation($"Queue Name: {queueName}");
-                message.Headers.FirstOrDefault(x => {
+                var route = message.Headers.FirstOrDefault(x => {
                     x.Fields.TryGetValue("Active", out string active);
                     return x.Name == "route-header" && active.ToLower() == "true";
-                }).Fields["Active"] = "false";
+                });
+                route.Fields["Active"] = "false";
+                route.Fields.Add("SentTimestamp", DateTime.UtcNow.ToString("yyyy-MM-ddTHH:mm:ss.fffZ"));
+                var current = message.Headers.FirstOrDefault(x => x.Name.Equals("current-queue-header"));
+                current.Fields["Name"] = queueName;
+                current.Fields["Timestamp"] = DateTime.UtcNow.ToString("yyyy-MM-ddTHH:mm:ss.fffZ");
                 await QueueMessageAsync(queueName, message, log);
             }
+
+        }
+
+        private async Task SendLog(Message message)
+        {
+            var idHeader = message.Headers.FirstOrDefault(x => x.Name.Equals("id-header"));
+            var current = message.Headers.FirstOrDefault(x => x.Name.Equals("current-queue-header"));
+            LogTimer logTimer = new LogTimer() {
+                Id = Guid.Parse(idHeader.Fields["GUID"]),
+                Queue = current.Fields["Name"],
+                SentTimestamp = DateTime.ParseExact(current.Fields["Timestamp"], "yyyy-MM-ddTHH:mm:ss.fffZ", CultureInfo.InvariantCulture),
+                ReceiveTimestamp = DateTime.UtcNow
+            };
+            await using var client = new ServiceBusClient(Environment.GetEnvironmentVariable("rasputinServicebus"));
+            ServiceBusSender sender = client.CreateSender("ms-logtimer");
+            string queueMessage = JsonSerializer.Serialize(logTimer, new JsonSerializerOptions
+                {
+                    PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+                });            
+            var messageBytes = Encoding.UTF8.GetBytes(queueMessage);
+            ServiceBusMessage messageObject = new ServiceBusMessage(messageBytes);
+
+            CancellationTokenSource cancellationTokenSource = new CancellationTokenSource(TimeSpan.FromSeconds(20));
+            CancellationToken cancellationToken = cancellationTokenSource.Token;
+
+            await sender.SendMessageAsync(messageObject, cancellationToken);
+            await sender.CloseAsync();
 
         }
 
@@ -41,7 +76,6 @@ namespace Rasputin.Router
         {
             await using var client = new ServiceBusClient(Environment.GetEnvironmentVariable("rasputinServicebus"));
             ServiceBusSender sender = client.CreateSender(queueName);
-
             string queueMessage = JsonSerializer.Serialize(message, new JsonSerializerOptions
                 {
                     PropertyNamingPolicy = JsonNamingPolicy.CamelCase
@@ -53,6 +87,7 @@ namespace Rasputin.Router
             CancellationToken cancellationToken = cancellationTokenSource.Token;
 
             await sender.SendMessageAsync(messageObject, cancellationToken);
+            await sender.CloseAsync();
         }
     }
 }
