b17ac0929474d997a3da9d0c27c78df355ed759d
rasputin <thomas.1.andersen@gmail.com>
2023-04-02 14:46:12 +0200

    Changed to servicebus queues

--- RasputinRouter/QueueTriggerRouter.cs
+++ RasputinRouter/QueueTriggerRouter.cs
@@ -1,11 +1,13 @@
 using System;
 using System.Linq;
+using System.Text;
 using System.Text.Json;
+using System.Threading;
 using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
 using Microsoft.Azure.Storage;
 using Microsoft.Azure.Storage.Queue;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
 
 namespace Rasputin.Router
@@ -13,7 +15,7 @@ namespace Rasputin.Router
     public class QueueTriggerRouter
     {
         [FunctionName("QueueTriggerRouter")]
-        public async Task RunAsync([QueueTrigger("api-router", Connection = "rasputinstorageaccount_STORAGE")]string myQueueItem, ILogger log)
+        public async Task RunAsync([ServiceBusTrigger("api-router", Connection = "rasputinServicebus")]string myQueueItem, ILogger log)
         {
             log.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
             var message = JsonSerializer.Deserialize<Message>(myQueueItem, new JsonSerializerOptions
@@ -37,19 +39,20 @@ namespace Rasputin.Router
 
         private async Task QueueMessageAsync(string queueName, Message message, ILogger log)
         {
-            // Get a reference to the queue
-            var str = Environment.GetEnvironmentVariable("rasputinstorageaccount_STORAGE");
-            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(str);
-            CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
-            CloudQueue queue = queueClient.GetQueueReference(queueName);
+            await using var client = new ServiceBusClient(Environment.GetEnvironmentVariable("rasputinServicebus"));
+            ServiceBusSender sender = client.CreateSender(queueName);
 
-            // Create a new message and add it to the queue
-            CloudQueueMessage queueMessage = new CloudQueueMessage(JsonSerializer.Serialize(message, new JsonSerializerOptions
+            string queueMessage = JsonSerializer.Serialize(message, new JsonSerializerOptions
                 {
                     PropertyNamingPolicy = JsonNamingPolicy.CamelCase
-                })
-                );
-            await queue.AddMessageAsync(queueMessage);
+                });            
+            var messageBytes = Encoding.UTF8.GetBytes(queueMessage);
+            ServiceBusMessage messageObject = new ServiceBusMessage(messageBytes);
+
+            CancellationTokenSource cancellationTokenSource = new CancellationTokenSource(TimeSpan.FromSeconds(20));
+            CancellationToken cancellationToken = cancellationTokenSource.Token;
+
+            await sender.SendMessageAsync(messageObject, cancellationToken);
         }
     }
 }
